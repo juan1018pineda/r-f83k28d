0deb74eb638f7054f684b31481abaf2b3778153a
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-14 20:32:46 -0500

    Counter created as state, increase method created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.increase = this.increase.bind(this);
+    this.state = {
+      counter: 0,
+    };
+  }
+  
+  increase() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.increase}>
+          Incrementa
+        </button>
       </div>
     );
   }
